d8a6220f172fbae82816aa73548e885c8f978fc8
gyuugoro <06_sober_sausage@icloud.com>
2023-09-21 18:45:55 +0900

    今度こそfirebase導入完了だろ！

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    "~/plugins/firebase.js"
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -50,5 +49,15 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
   },
+
+  env: {
+    fb_api_key: process.env.FB_API_KEY,
+    fb_auth_domain: process.env.FB_AUTH_DOMAIN,
+    fb_project_id: process.env.FB_PROJECT_ID,
+    fb_storage_bucket: process.env.FB_STORAGE_BUCKET,
+    fb_messaging_sender_id: process.env.FB_MESSAGING_SENDER_ID,
+    fb_app_id: process.env.FB_APP_ID,
+    fb_measurement_id: process.env.FB_MEASUREMENT_ID
+  }
 
 }
--- pages/books/_bookid/index.vue
+++ pages/books/_bookid/index.vue
@@ -1,10 +1,25 @@
 <template>
-  <nuxt-child />
+  <div>
+    {{ text }}
+  </div>
 </template>
 
 <script>
-export default {
+import { get_book_id } from "../../../firebase"
 
+export default {
+  data() {
+    return {
+      text: "失敗"
+    }
+  },
+  created() {
+    (async () => {
+      const data = await get_book_id("owirttFmsMyrs7F2MKoo")
+
+      this.text = data.name
+    })()
+  }
 }
 </script>
 
