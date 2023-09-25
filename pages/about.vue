acbef23841c89334c171cdb28ef355ee73008dfc
gyuugoro <06_sober_sausage@icloud.com>
2023-09-25 20:24:06 +0900

    一旦これで完成とする

--- pages/about.vue
+++ pages/about.vue
@@ -5,17 +5,21 @@
 
       <p>
         このWebアプリは
-        <a href="https://instagram.com/shotaro20060930">@shotaro20060930</a>
+        <a href="https://instagram.com/shotaro20060930" target="_blank">@shotaro20060930</a>
         が制作した単語帳アプリで、誰でも無料で使用することができます。
       </p>
 
       <p>バグなど不具合の報告は僕のInstagramにお願いします。</p>
-      <a href="https://instagram.com/shotaro20060930?igshid=NzZlODBkYWE4Ng=="
+      <a href="https://instagram.com/shotaro20060930?igshid=NzZlODBkYWE4Ng==" target="_blank"
         class="button is-rounded is-fullwidth block is-danger is-light is-outlined">Instagram</a>
 
       <p>コードはここに公開しているので、有識者の方！アドバイスをください！</p>
-      <a href="https://github.com/gyuugoro/ankipan2"
+      <a href="https://github.com/gyuugoro/ankipan2" target="_blank"
         class="button is-rounded is-fullwidth block is-dark is-light is-outlined">Github</a>
+
+      <p>単語帳はここから誰でも追加できます。</p>
+      <a href="https://ankipan2-make.vercel.app/" target="_blank"
+        class="button is-rounded is-fullwidth block is-success is-light is-outlined">作成する</a>
 
 
       <h2>使用言語</h2>
@@ -24,23 +28,23 @@
       <h2>使用技術</h2>
 
       <h3>Nuxt.js</h3>
-      <a href="https://v2.nuxt.com/">https://v2.nuxt.com/</a>
+      <a href="https://v2.nuxt.com/" target="_blank">https://v2.nuxt.com/</a>
       <p>ブラウザでの処理は全てNuxt.js上で行っています。</p>
 
       <h3>Firebase</h3>
-      <a href="https://firebase.google.com/">https://firebase.google.com/</a>
+      <a href="https://firebase.google.com/" target="_blank">https://firebase.google.com/</a>
       <p>サーバーでの処理は全てFirebase上で行っています。無料で使わせてもらっています。感謝！</p>
 
       <h3>Vercel</h3>
-      <a href="https://vercel.com/">https://vercel.com/</a>
+      <a href="https://vercel.com/" target="_blank">https://vercel.com/</a>
       <p>ドメインを無料で使わせてもらっています。感謝！</p>
 
       <h3>PWA</h3>
-      <a href="https://web.dev/progressive-web-apps/">https://web.dev/progressive-web-apps/</a>
+      <a href="https://web.dev/progressive-web-apps/" target="_blank">https://web.dev/progressive-web-apps/</a>
       <p>ギガをくわないようにするため、オフラインでも動作するようにしました。</p>
 
       <h3>Bulma</h3>
-      <a href="https://bulma.io/">https://bulma.io/</a>
+      <a href="https://bulma.io/" target="_blank">https://bulma.io/</a>
       <p>デザインはBulmaをベースにしています。</p>
 
     </div>
