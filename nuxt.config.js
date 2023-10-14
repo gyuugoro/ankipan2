9e98734978f73c38891e6df3d11b921a67c24177
gyuugoro <06_sober_sausage@icloud.com>
2023-10-14 16:44:56 +0900

    Google検索最適化

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '単語帳アプリ' },
+      { hid: 'description', name: 'description', content: 'Welcome to Ankipan2!. Ankipan2は@shotaro20060930(インスタ)が制作した単語帳アプリで、誰でも無料で使用することができます。' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [
